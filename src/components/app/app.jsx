04314edfb79588bbc8fc3f8c0dcb80e0e1f0fb9b
EvgputWeb <EvgputWeb@yandex.ru>
2019-10-20 22:54:20 +0300

    Подключил PropTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-export default function App() {
+
+export default function App(props) {
+
+  const {placesList} = props;
+
+  const places = placesList.map((place, index) =>
+    <article key={index} className="cities__place-card place-card">
+      <div className="place-card__mark">
+        <span>Premium</span>
+      </div>
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;120</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
+        </div>
+
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: `93%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{place}</a>
+        </h2>
+        <p className="place-card__type">Apartment</p>
+      </div>
+
+    </article>
+  );
+
   return (
+
     <div className="page page--gray page--main">
 
       <header className="header">
@@ -27,8 +73,10 @@ export default function App() {
         </div>
       </header>
 
-      <main className="page__main page__main--index page__main--index-empty">
+      <main className="page__main page__main--index">
+
         <h1 className="visually-hidden">Cities</h1>
+
         <div className="tabs">
           <section className="locations container">
             <ul className="locations__list tabs__list">
@@ -48,7 +96,7 @@ export default function App() {
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locations__item-link tabs__item">
+                <a className="locations__item-link tabs__item tabs__item--active">
                   <span>Amsterdam</span>
                 </a>
               </li>
@@ -58,27 +106,58 @@ export default function App() {
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active" href="#">
+                <a className="locations__item-link tabs__item" href="#">
                   <span>Dusseldorf</span>
                 </a>
               </li>
             </ul>
           </section>
         </div>
+
         <div className="cities">
-          <div className="cities__places-container cities__places-container--empty container">
-            <section className="cities__no-places">
-              <div className="cities__status-wrapper tabs__content">
-                <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
+          <div className="cities__places-container container">
+
+            <section className="cities__places places">
+              <h2 className="visually-hidden">Places</h2>
+              <b className="places__found">312 places to stay in Amsterdam</b>
+              <form className="places__sorting" action="#" method="get">
+                <span className="places__sorting-caption">Sort by</span>
+                <span className="places__sorting-type" tabIndex="0">
+                  Popular
+                  <svg className="places__sorting-arrow" width="7" height="4">
+                    <use xlinkHref="#icon-arrow-select"></use>
+                  </svg>
+                </span>
+                <ul className="places__options places__options--custom places__options--opened">
+                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
+                  <li className="places__option" tabIndex="0">Price: low to high</li>
+                  <li className="places__option" tabIndex="0">Price: high to low</li>
+                  <li className="places__option" tabIndex="0">Top rated first</li>
+                </ul>
+              </form>
+              <div className="cities__places-list places__list tabs__content">
+
+                {places}
+
               </div>
             </section>
-            <div className="cities__right-section"></div>
+
+            <div className="cities__right-section">
+              <section className="cities__map map"></section>
+            </div>
+
           </div>
+
         </div>
+
       </main>
 
     </div>
+
   );
 }
 
+
+App.propTypes = {
+  placesList: PropTypes.arrayOf(PropTypes.string).isRequired
+};
